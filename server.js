060f1e21a4a74b1d769d2425ec566a5cc3491675
leonardo <leonardo.14117125@student.itera.ac.id>
2021-12-13 06:07:50 +0700

    change baseurl

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ const config = {
 
 const port = process.env.PORT || 3000;
 if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
-  config.baseURL = `http://localhost:${port}`;
+  config.baseURL = `http://143.244.150.6:${port}`;
 }
 
 app.use(auth(config));
